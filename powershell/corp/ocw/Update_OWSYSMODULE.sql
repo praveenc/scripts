4a2e0efec2e6ef02c8e14ce4368522f9f168485e
Praveen Chamarthi <chamarthi@outlook.com>
2015-02-07 07:02:35 -0500

    deleted corp dir

--- powershell/corp/ocw/Update_OWSYSMODULE.sql
+++
@@ -1,2 +0,0 @@
-PRINT 'Processing OwSysModule insert'
-INSERT INTO [dbo].[OwSysModule]([ModuleName],[InstalledFlag],[VersionNo],[ObjectType],[UpdateDate]) VALUES ('CORE',1,'2.0.0.109','CombinePackage',GETDATE())
